c663c17cf1a5256c541597dee0bfde79ba359301
witjmc <106864033+witjmc@users.noreply.github.com>
2024-02-20 21:23:18 +0900

    Create TodoList.vue

---
+++ src/components/TodoList.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="row">
+    <div class="col">
+      <ul class="list-group">
+        <TodoListItem
+          v-for="todoItem in todoList"
+          :key="todoItem.id"
+          :todoItem="todoItem"
+          @delete-todo="deleteTodoHandler"
+          @toggle-completed="toggleCompletedHandler"
+        />
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import TodoListItem from './TodoListItem.vue'
+import { defineProps, defineEmits } from 'vue'
+
+// eslint-disable-next-line no-unused-vars
+const props = defineProps({
+  todoList: { type: Array, required: true }
+})
+
+const { emit } = defineEmits(['delete-todo', 'toggle-completed'])
+
+const deleteTodoHandler = (id) => {
+  emit('delete-todo', id)
+}
+
+const toggleCompletedHandler = (id) => {
+  emit('toggle-completed', id)
+}
+</script>
